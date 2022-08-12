62eb6a7401cf69bedcd7c38aeeea61ea45ff2980
oladipo155 <thinktankexpress@gmail.com>
2022-08-12 13:54:19 +0100

    weryth

---
+++ src/components/StudentContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class StudentContainer extends React.Component{
+    render(){
+        return(
+            <>
+            <hi>Hello from Torilo world!</hi>
+            <p>I am the parent component</p>
+            </>
+        )
+    }
+}
+export default StudentContainer;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StudentContainer from './components/StudentContainer';
+
+//const element = <h1>Welcome to the the torilo student mgt app!</h1>
+ReactDOM.render(
+<React.StrictMode>
+    <StudentContainer />
+</React.StrictMode>,
+ document.getElementById('root'));
